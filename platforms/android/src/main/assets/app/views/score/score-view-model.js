e60173c8af4b7f60a9f8738c6f17318d625e22d2
dimitarsd <dsd363978@yahoo.com>
2016-02-17 15:06:42 +0200

    Implemented score page - added UI and showing data (need to get the data from Telerik Backend Services)

--- platforms/android/src/main/assets/app/views/score/score-view-model.js
+++ platforms/android/src/main/assets/app/views/score/score-view-model.js
@@ -1,6 +1,24 @@
 var observable = require("data/observable");
 
+var scoreWorldwidePropName = 'scoreWorldwide';
+var scoreLocalPropName = 'scoreLocal';
+
 var ScoreModel = new observable.Observable();
+
+var worldwide = [];
+worldwide.push({name: 'Nikolay', score: 350, country: 'Bulgaria'});
+worldwide.push({name: 'John', score: 85, country: 'USA'});
+worldwide.push({name: 'Pablo', score: 60, country: 'Mexico'});
+worldwide.push({name: 'Dmitrii', score: 10, country: 'Russia'});
+
+var local = [];
+local.push({name: 'Vasil', score: 150, country: 'Bulgaria'});
+local.push({name: 'Ivan', score: 145, country: 'Bulgaria'});
+local.push({name: 'Adrian', score: 70, country: 'Bulgaria'});
+local.push({name: 'Ivaylo', score: 55, country: 'Bulgaria'});
+
+ScoreModel.set(scoreWorldwidePropName, worldwide);
+ScoreModel.set(scoreLocalPropName, local);
 
 // TODO: Get score statistics
 // top 3 worldwide 
